b01f4aa5da09689b6bc06a54bb8ffe87cf2cb327
git12nazneen <lipinazneen65@gmail.com>
2024-11-20 11:28:57 +0600

    added route

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Index page</h1>
+        
+        <about></about>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Producta</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
