c19ec68d662bb01fba116c3c5e96b270409f8f33
git12nazneen <lipinazneen65@gmail.com>
2024-11-20 13:51:34 +0600

    added details page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,78 @@
 <template>
-    <div>
-        <h1>Products details {{ id }}</h1>
+    <div v-if="singleproduct">
+        <div class="max-w-5xl mx-auto my-20">
+            
+            <div
+                class="bg-white border rounded-xl shadow-sm sm:flex dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                <div
+                    class="shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-s-xl sm:max-w-60 md:rounded-se-none md:max-w-xs">
+                    <img :src="singleproduct.image" :alt="singleproduct.title"
+                        class="size-full absolute top-0 start-0 object-cover">
+                </div>
+                <div class="flex flex-wrap">
+                    <div class="p-4 flex flex-col h-full sm:p-7">
+                        <h1 className="text-2xl font-bold text-gray-800 mb-2">{{ singleproduct.title }}</h1>
+                        <p className="text-gray-600 mb-4">{{ singleproduct.description }}</p>
+                        <div className="flex justify-between items-center mb-4">
+                            <span className="text-lg font-semibold text-orange-600">${{ singleproduct.price }}</span>
+                            <span className="text-sm text-gray-500">Category: {{ singleproduct.category }}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <span className="text-yellow-500 font-bold text-lg">
+                                ⭐ {{ singleproduct.rating.rate }}
+                            </span>
+                            <span className="text-gray-500 text-sm ml-2">
+                                ({{ singleproduct.rating.count }}reviews)
+                            </span>
+                        </div>
+                        <div class="mt-5 sm:mt-auto">
+                            <div class="px-5 mt-2 py-2.5 relative rounded group  text-white font-medium inline-block">
+                                <span
+                                    class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                                <span
+                                    class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                                <span
+                                    class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                                <span
+                                    class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                                <span class="relative">Add to card</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <div v-else>
+        <p>Loading product details...</p>
     </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+// Route parameter
 const { id } = useRoute().params
+
+const singleproduct = ref(null)
+
+const fetchProduct = async () => {
+    const uri = `https://fakestoreapi.com/products/${id}`
+    try {
+        const response = await fetch(uri)
+        if (!response.ok) throw new Error('Failed to fetch product data')
+        singleproduct.value = await response.json()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+onMounted(() => {
+    fetchProduct()
+})
 definePageMeta({
     layout: 'product'
 })
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,27 +2,31 @@
     <div class="container mx-auto p-10">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             <div v-for="product in products">
-                <a class="flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg focus:outline-none focus:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 h-[440px]"
-                    >
-                    <img class="w-full h-auto rounded-t-xl"
-                        src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80"
-                        alt="Card Image">
+                <a
+                    class="flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg focus:outline-none focus:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 h-[480px]">
+
+                    <img :src="product.image" :alt="product.title" class="w-full h-72 p-7 rounded-t-xl" />
                     <div class="p-4 md:p-5">
                         <h3 class="text-lg font-bold text-gray-800 dark:text-white">
-                           {{ product.title.slice(0,20) }}
+                            {{ product.title.slice(0, 20) }}
                         </h3>
                         <p class="mt-1 text-gray-500 dark:text-neutral-400">
-                           {{ product.description.slice(0,160) }}
+                            {{ product.description.slice(0, 110) }}
                         </p>
                         <button>
                             <NuxtLink :to="`/products/${product.id}`">
-                                <a href="#_" class="px-5 mt-2 py-2.5 relative rounded group font-medium text-white font-medium inline-block">
-    <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
-    <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
-    <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
-    <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-    <span class="relative">See Details</span>
-</a>
+                                <div
+                                    class="px-5 mt-2 py-2.5 relative rounded group  text-white font-medium inline-block">
+                                    <span
+                                        class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                                    <span
+                                        class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                                    <span
+                                        class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                                    <span
+                                        class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                                    <span class="relative">See Details</span>
+                                </div>
                             </NuxtLink>
                         </button>
                     </div>
