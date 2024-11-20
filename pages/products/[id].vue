089611fc3e7c2e7f6684f5e35aaa249d88866525
git12nazneen <lipinazneen65@gmail.com>
2024-11-20 13:57:06 +0600

    added button reuser:

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="singleproduct">
         <div class="max-w-5xl mx-auto my-20">
-            
+
             <div
                 class="bg-white border rounded-xl shadow-sm sm:flex dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
                 <div
@@ -26,17 +26,7 @@
                             </span>
                         </div>
                         <div class="mt-5 sm:mt-auto">
-                            <div class="px-5 mt-2 py-2.5 relative rounded group  text-white font-medium inline-block">
-                                <span
-                                    class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
-                                <span
-                                    class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
-                                <span
-                                    class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
-                                <span
-                                    class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-                                <span class="relative">Add to card</span>
-                            </div>
+                           <Gbutton>Add To Cart</Gbutton>
                         </div>
                     </div>
                 </div>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -15,20 +15,11 @@
                         </p>
                         <button>
                             <NuxtLink :to="`/products/${product.id}`">
-                                <div
-                                    class="px-5 mt-2 py-2.5 relative rounded group  text-white font-medium inline-block">
-                                    <span
-                                        class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
-                                    <span
-                                        class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
-                                    <span
-                                        class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
-                                    <span
-                                        class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-                                    <span class="relative">See Details</span>
-                                </div>
+                                <Gbutton> See Details</Gbutton>
                             </NuxtLink>
+
                         </button>
+
                     </div>
                 </a>
             </div>
