74a512dffae8d21cedab304c434517ae3b093ffe
git12nazneen <lipinazneen65@gmail.com>
2024-11-20 19:24:59 +0600

    added nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,8 +2,7 @@
     <div>
         <header class=" flex items-center justify-between  px-6 py-4 mx-auto rounded bg-slate-100 dark:bg-gray-50">
             <NuxtLink to="/" rel="noopener noreferrer" aria-label="Homepage">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                    class="w-6 h-6 dark:text-violet-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 dark:text-violet-600">
                     <path
                         d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z">
                     </path>
@@ -13,7 +12,7 @@
                 </svg>
             </NuxtLink>
             <div class="items-center hidden space-x-8 lg:flex">
-                <div class="space-x-4 flex justify-between ">
+                <div class="space-x-10 flex justify-between list-none">
                     <li>
                         <NuxtLink to="/">Home</NuxtLink>
                     </li>
@@ -24,8 +23,9 @@
                         <NuxtLink to="/products">Product</NuxtLink>
                     </li>
                 </div>
-               <Gbutton>Sign Up</Gbutton>
+                <Gbutton>Sign Up</Gbutton>
             </div>
+
             <button class="flex items-center justify-center p-2 lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
                     class="w-6 h-6 dark:text-gray-900">
@@ -37,12 +37,10 @@
     </div>
 </template>
 
-<script >
+<script>
 export default {
     name: "Navbar"
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
